8716e71e21f65ab8108b893b277d79efb5891a1a
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-03-29 02:48:19 +0700

    feat: contact detail

--- src/pages/contact/:contactId.tsx
+++ src/pages/contact/:contactId.tsx
@@ -1,5 +1,34 @@
+import { LoaderFunction, useLoaderData } from 'react-router-dom';
+import client from '~/configs/graphql';
+import { GET_CONTACT_DETAIL } from '~/services';
+import { LoaderData } from '~/types/common.type';
+import { Contact } from '~/types/models.interface';
+
 export default function ContactDetail() {
-  return <div>contact detail</div>;
+  const {
+    data: { contact_by_pk: contact },
+  } = useLoaderData() as LoaderData<typeof loader>;
+
+  return (
+    <div>
+      <p>
+        {contact.first_name} {contact.last_name}
+      </p>
+      <p>Phone Number:</p>
+      {contact.phones.map((phone) => (
+        <div key={phone.number}>{phone.number}</div>
+      ))}
+    </div>
+  );
 }
 
-export const loader = () => {};
+export const loader = (async ({ params, request }) => {
+  const res = await client.query<{ contact_by_pk: Contact }>({
+    query: GET_CONTACT_DETAIL,
+    variables: {
+      id: params.contactId,
+    },
+  });
+  console.log(res);
+  return res;
+}) satisfies LoaderFunction;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import client from '~/configs/graphql';
-import { LoaderFunction, useLoaderData } from 'react-router-dom';
+import { LoaderFunction, useLoaderData, useNavigate } from 'react-router-dom';
 import { Contact } from '~/types/models.interface';
 import { LoaderData } from '~/types/common.type';
 import { GET_CONTACTS } from '~/services';
 
 const ContactList = () => {
+  const navigate = useNavigate();
   const data = useLoaderData() as LoaderData<typeof loader>;
 
   return (
     <div>
       {data.data.contact.map((c) => (
-        <div key={c.id}>
+        <div
+          key={c.id}
+          onClick={() => navigate(`/contact/${c.id}`)}
+          onKeyDown={(e) => e.preventDefault()}
+        >
           <p>
             {c.first_name} {c.last_name}
           </p>
